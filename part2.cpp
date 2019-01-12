4d1a07c4c41a3f6002e16706138186b185067ac5
Erin <lixinru0415@outlook.com>
2019-01-12 12:28:08 -0500

    create class for graph

---
+++ part2.cpp
@@ -0,0 +1,45 @@
+// part2
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <limits.h>
+#include <algorithm>
+#include <utility>
+
+using namespace std;
+
+class Graph;
+class Edege;
+class Vertex;
+
+class Edge {
+    int weight;
+    Vertex* v1;
+    Vertex* v2;
+public:
+    int getWeight() const {return weight;}
+    Vertex* getV1() const {return v1;}
+    Vertex* getV2() const {return v2;}
+    void setWeight(int w) {weight = w;}
+    void setV1(Vertex* v) {v1 = v;}
+    void setV2(Vertex* v) {v2 = v;}
+    Edge(int w = INT_MAX, Vertex* V1, Vertex* V2) {weight = w; v1 = V1; v2 = V2;}
+
+};
+
+class Vertex {
+    bool visited;
+    vector<Edge *> 
+};
+
+class Graph {
+    vector<vector<Vertex>> adjList;
+public:
+    
+
+}
+
+int main() {
+
+}
