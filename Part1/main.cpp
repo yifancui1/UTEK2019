44d2bd51165a8469aeed30feb7727ea40089f230
ziyuan <jerrywang.wang@mail.utoronto.ca>
2019-01-12 12:48:14 -0500

    part1 v1.0

--- Part1/main.cpp
+++ Part1/main.cpp
@@ -12,15 +12,139 @@
  */
 
 #include <cstdlib>
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <cstdlib>
 
 using namespace std;
 
 /*
  * 
  */
-/asdfasdf
+
+class product {
+private:
+    int x;
+    int y;
+    int productNum;
+    double weight;
+    int qty;
+    
+public:
+    void setQty(int x){
+        qty = x;
+    }
+    int getProductNum(){
+        return productNum;
+    }
+    int getX(){
+        return x;
+    }
+    int getY(){
+        return y;
+    }
+    double getWeight(){
+        return weight;
+    }
+    int getQty(){
+        return qty;
+    }
+    product (int x, int y, int productNum, double weight, int qty){
+        this->x = x;
+        this->y = y;
+        this->productNum = productNum;
+        this->weight = weight;
+        this->qty = qty;
+    }
+    void setX(int x){
+        this->x = x;
+    }
+    void setY(int y){
+        this->y = y;
+    }
+    
+    void setProductNum(int productNum){
+        this->productNum = productNum;
+    }
+    void setWeight (double weight){
+        this->weight = weight;
+    }
+    
+    product(){
+    }
+};
+
 int main(int argc, char** argv) {
 
+    vector <product> v;
+    
+    ifstream myfile;
+    myfile.open("1a.in");
+    
+    string line;
+    while (getline(myfile, line)){
+        char a;
+        int x;
+        int y;
+        int productNum;
+        double weight;
+        line >> a;
+        line >> x;
+        line >> a;
+        line >> y;
+        line >> a;
+        line >> productNum;
+        line >> a;
+        line >> weight;
+        line >> a;
+        
+        if (!v.empty()){
+            for (int i = 0; i < v.size(); i++){
+                if (v[i].getX() == x && v[i].getY() == y && v[i].getProductNum() == productNum && v[i].getWeight() == weight)
+                    v[i].setQty(v[i].getQty() + 1);
+            }
+        }
+        else{
+            product newProduct(x, y, productNum, weight, 1);
+            v.pushback(newProduct);
+        }
+    }
+    
+    sort (v, v.size());
+    
+    //print
+    
+    
     return 0;
 }
 
+void sort (vector <product> & v, int n){
+    int i, j;
+    for (i = 0; i < n - 1; i++){
+        for (j = 0; j < n - i -1; j++){
+            if (v[j].getProductNum() > v[j+1].getProductNum())
+                swap (v[j], v[j+1]);
+                
+        }
+    }
+}
+
+
+void swap (product &p1, product &p2){
+    product temp (p1.getX(), p1.getY(), p1.getProductNum(), p1.getWeight(), p1.getQty());
+    p1.setX(p2.getX());
+    p1.setY(p2.getY());
+    p1.setProductNum(p2.getProductNum());
+    p1.setWeight(p2.getWeight());
+    p1.setQty(p2.getQty());
+    
+    p2.setX(temp.getX());
+    p2.setY(temp.getY());
+    p2.setProductNum(temp.getProductNum());
+    p2.setWeight(temp.getWeight());
+    p2.setQty(temp.getQty());
+    
+}
